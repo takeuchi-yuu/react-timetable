8ef00140e88ff488f792c7f6392b1df271847a47
takeuchi-yuu <zhuneikaifayong@gmail.com>
2025-03-20 13:21:47 +0900

    各ページへのルーティングを実装

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Counter from "./Counter";
+import Timetable from "./pages/Timetable";
+import Classes from "./pages/Classes";
+import ClassEdit from "./pages/ClassEdit";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/timetable" element={<Timetable />}></Route>
+        <Route path="/classes" element={<Classes />}></Route>
+        <Route path="/class-edit" element={<ClassEdit />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </BrowserRouter>
   );
