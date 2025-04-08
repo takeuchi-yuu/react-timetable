3bfc1614209557c070872f28d21292f9c3a71a0e
takeuchi-yuu <zhuneikaifayong@gmail.com>
2025-04-08 11:57:04 +0900

    メインページへのリンクを追加しました

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
   return (
     <div className="not-found">
       <h1>存在しないページです</h1>
+      <Link to="/timetable">時間割トップへ</Link>
     </div>
   );
 };
